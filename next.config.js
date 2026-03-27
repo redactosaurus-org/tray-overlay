4471332f893c0c2191c15ced4714f19809ef4846
kishon45229 <kishon45229@outlook.com>
2026-03-27 13:02:47 +0530

    Set output: "export" for a static build (index.html site) instead of standalone.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   distDir: "dist",
-  output: "standalone",
+  output: "export", // static HTML export mode instead of server bundle
 };
 
 module.exports = nextConfig;
